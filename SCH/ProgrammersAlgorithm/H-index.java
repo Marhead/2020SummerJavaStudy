dbe1e6bf22888a0fc56c9a50cc561d774fd5b5c2
Marhead <clgns0415@gmail.com>
2020-07-22 12:15:30 +0900

    Update H-index.java

--- SCH/ProgrammersAlgorithm/H-index.java
+++ SCH/ProgrammersAlgorithm/H-index.java
@@ -1,6 +1,20 @@
+import java.util.Arrays;
+
 class Solution {
     public int solution(int[] citations) {
         int answer = 0;
+        int h = 0;
+
+        Arrays.sort(citations);
+
+        for(int i = 1; i < citations.length; i++)
+        {
+            if(citations[citations.length - i] == citations.length - citations[citations.length - i] + 1)
+            {
+                answer = citations[citations.length - i];
+            }
+        }
+
         return answer;
     }
 }
