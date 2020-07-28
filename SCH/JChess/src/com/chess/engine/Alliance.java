d98b9eb193e46dcdad5b8653965489c41c79d86b
Marhead <clgns0415@gmail.com>
2020-07-28 16:15:09 +0900

    java until pawn part 2

---
+++ SCH/JChess/src/com/chess/engine/Alliance.java
@@ -0,0 +1,18 @@
+package com.chess.engine;
+
+public enum Alliance {
+    WHITE {
+        @Override
+        public int getDirection() {
+            return -1;
+        }
+    },
+    BLACK {
+        @Override
+        public int getDirection() {
+            return 1;
+        }
+    };
+
+    public abstract int getDirection();
+}
--- SCH/JChess/src/com/chess/engine/board/Tile.java
+++ SCH/JChess/src/com/chess/engine/board/Tile.java
@@ -19,7 +19,7 @@ public abstract class Tile {
 
         final Map<Integer, EmptyTile> emptyTileMap = new HashMap<>();
 
-        for(int i = 0; i < 64; i++) {
+        for(int i = 0; i < BoardUtils.NUM_TILES; i++) {
             emptyTileMap.put(i, new EmptyTile(i));
         }
 
@@ -30,7 +30,7 @@ public abstract class Tile {
         return piece != null ? new OccupiedTile(tileCoordinate, piece) : EMPTY_TILES_CACHE.get(tileCoordinate);
     }
 
-    private Tile(int tileCoordinate) {
+    private Tile(final int tileCoordinate) {
         this.tileCoordinate = tileCoordinate;
     }
 
@@ -59,7 +59,7 @@ public abstract class Tile {
 
         private final Piece pieceOnTile;
 
-        private OccupiedTile(int tileCoordinate, Piece pieceOnTile){
+        private OccupiedTile(int tileCoordinate, final Piece pieceOnTile){
             super(tileCoordiante);
             this.pieceOnTile = pieceOnTile;
         }
