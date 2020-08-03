c14272e47376912867b3f562ef94e011198df333
HyunKi Kim <gusrl4025@naver.com>
2020-08-03 22:29:55 +0900

    knight finished really

--- KHG/JChess/src/com.chess.engine/board/BoardUtils.java
+++ KHG/JChess/src/com.chess.engine/board/BoardUtils.java
@@ -2,17 +2,29 @@ package com.chess.engine.board;
 
 public class BoardUtils {
 
-    public static final boolean[] FIRST_COLUMN = null;
-    public static final boolean[] SECOND_COLUMN = null;
-    public static final boolean[] SEVENTH_COLUMN = null;
-    public static final boolean[] EIGHTH_COLUMN = null;
+    public static final boolean[] FIRST_COLUMN = initColumn(0);
+    public static final boolean[] SECOND_COLUMN = initColumn(1);
+    public static final boolean[] SEVENTH_COLUMN = initColumn(2);
+    public static final boolean[] EIGHTH_COLUMN = initColumn(7);
+
+    public static final int NUM_TILES = 64;
+    public static final int NUM_TILES_PER_ROW = 8;
 
     /* 다른 사람이 접근하려 할 때 오류가 발생함 */
     private BoardUtils() {
         throw new RuntimeException("You cannot instantiate me!");
     }
 
-    public static boolean isValidTileCoordinate(int coordinate) {
+    private static boolean[] initColumn(int columnNumber) {
+        final boolean[] column = new boolean[64];
+        do {
+            column[columnNumber] = true;
+            columnNumber += 8;
+        } while(columnNumber < 64);
+        return column;
+    }
+
+    public static boolean isValidTileCoordinate(final int coordinate) {
         return coordinate >= 0 && coordinate < 64;
     }
 }
