a5c18b2a527aeefa8ea31d3a53bb19f60b6758d4
BaeLuv <59927119+BaeLuv@users.noreply.github.com>
2020-09-26 01:30:15 +0900

    Add files via upload

---
+++ KHB/TourSolver.java
@@ -0,0 +1,128 @@
+/*
+ * Name: Kim Hyun Bae
+ * Student ID #: 2017145086
+ */
+
+/* 
+ * Do NOT import any additional packages/classes.
+ * If you (un)intentionally use some additional packages/classes we did not
+ * provide, you may receive a 0 for the homework.
+ */
+
+public final class TourSolver implements ITourSolver {
+    
+    int objective;
+    int boardwidth;
+    int boardheight;
+    int[][] copyboard;
+    int[][] movement = new int[][]{{-2,-1},{-2,1},{-1,-2},{-1,2},{1,-2},{1,2},{2,-1},{2,1}};
+    boolean findit = false;
+    int[] solution;
+
+    @Override
+    public int[] solve(Board board) {
+        
+        boardwidth = board.getWidth();
+        boardheight = board.getHeight();
+        copyboard = CopyBoard(board);
+        objective = SetObjective(copyboard);
+
+        for(int i=0; i<boardwidth; i++){
+            if(findit){
+                break;
+            }
+            for(int j=0; j<boardheight; j++){
+                if(isSpace(i,j)){
+                    copyboard[i][j] = 0;
+                    if(MoveRecursion(i,j,1,movement)){
+                        break;
+                    } else {
+                        copyboard[i][j] = -1;
+                    }
+                }
+            }
+        }
+
+        if(findit){
+            solution = SequenceMaker(copyboard);
+            return solution;
+        } else {
+            solution = new int[]{};
+            return solution;
+        }
+    }
+
+
+    public int[] SequenceMaker(int[][] arr){
+        
+        int[] savezone = new int[objective];
+        for(int i=0;i<boardwidth; i++){
+            for(int j=0; j<boardheight; j++){
+                if(arr[i][j]>=0){
+                    savezone[arr[i][j]] = i*boardheight + j;
+                }
+            }
+        }
+        return savezone;
+    }
+
+    public boolean MoveRecursion(int i, int j, int n, int[][] inputmovement) { 
+        if (n == objective){
+            findit = true;
+            return true;
+        }
+
+        for(int[] mov : inputmovement){
+            if(isSpace(i+mov[0],j+mov[1])){
+                copyboard[i+mov[0]][j+mov[1]] = n;
+                
+                if(MoveRecursion(i+mov[0],j+mov[1],n+1,inputmovement)){
+                    return true;
+                } else {
+                    copyboard[i+mov[0]][j+mov[1]] = -1;
+                }
+            }
+        }
+         
+        return false; 
+    }
+
+    public boolean isSpace(int i, int j){
+          
+        if(i<0 || i >= boardwidth || j <0 || j >= boardheight || copyboard[i][j] != -1){
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+
+    public int[][] CopyBoard(Board board){
+        int[][] tmpcopyboard = new int[boardwidth][boardheight];
+
+        for(int i=0; i<boardwidth; i++){
+            for(int j=0; j<boardheight; j++){
+                if(board.isMissing(i,j)){
+                    tmpcopyboard[i][j] = -2;
+                } else {
+                    tmpcopyboard[i][j] = -1;
+                }
+            }
+        }
+        return tmpcopyboard;
+    }
+   
+    public int SetObjective(int[][] somecopyboard){
+        int count = 0;
+        for(int i=0; i<boardwidth; i++){
+            for(int j=0; j<boardheight; j++){
+                if(somecopyboard[i][j] == -1){
+                    count++;
+                } 
+            }
+        }
+        return count;
+    }
+
+
+}
