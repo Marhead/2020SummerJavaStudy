feeec8bdc6885127a799ea9595c3eaa058580a96
BaeLuv <59927119+BaeLuv@users.noreply.github.com>
2020-09-26 02:29:10 +0900

    Update TourSolver.java

--- KHB/TourSolver.java
+++ KHB/TourSolver.java
@@ -14,9 +14,9 @@ public final class TourSolver implements ITourSolver {
     int objective;
     int boardwidth;
     int boardheight;
-    int[][] copyboard;
+    boolean[][] copyboard;
     int[][] movement = new int[][]{{-2,-1},{-2,1},{-1,-2},{-1,2},{1,-2},{1,2},{2,-1},{2,1}};
-    int[] solution;
+    int[] savezone;
 
     @Override
     public int[] solve(Board board) {
@@ -25,37 +25,24 @@ public final class TourSolver implements ITourSolver {
         boardheight = board.getHeight();
         copyboard = CopyBoard(board);
         objective = SetObjective(copyboard);
+        savezone = new int[objective];
 
         for(int i=0; i<boardwidth; i++){
             for(int j=0; j<boardheight; j++){
                 if(isSpace(i,j)){
-                    copyboard[i][j] = 0;
+                    copyboard[i][j] = true;
+                    savezone[0]=i*boardheight + j;
                     if(MoveRecursion(i,j,1)){
-                        solution = SequenceMaker(copyboard);
-                        return solution;
+                        return savezone;
                     } else {
-                        copyboard[i][j] = -1;
+                        copyboard[i][j] = false;
                     }
                 }
             }
         }
 
-        solution = new int[]{};
-        return solution;     
-    }
-
-
-    public int[] SequenceMaker(int[][] arr){
-        
-        int[] savezone = new int[objective];
-        for(int i=0;i<boardwidth; i++){
-            for(int j=0; j<boardheight; j++){
-                if(arr[i][j]>=0){
-                    savezone[arr[i][j]] = i*boardheight + j;
-                }
-            }
-        }
-        return savezone;
+        savezone = new int[]{};
+        return savezone;     
     }
 
     public boolean MoveRecursion(int i, int j, int n) { 
@@ -65,12 +52,12 @@ public final class TourSolver implements ITourSolver {
 
         for(int[] mov : movement){
             if(isSpace(i+mov[0],j+mov[1])){
-                copyboard[i+mov[0]][j+mov[1]] = n;
-                
+                copyboard[i+mov[0]][j+mov[1]] = true;
+                savezone[n]=(i+mov[0])*boardheight + j+mov[1];
                 if(MoveRecursion(i+mov[0],j+mov[1],n+1)){
                     return true;
                 } else {
-                    copyboard[i+mov[0]][j+mov[1]] = -1;
+                    copyboard[i+mov[0]][j+mov[1]] = false;
                 }
             }
         }
@@ -80,7 +67,7 @@ public final class TourSolver implements ITourSolver {
 
     public boolean isSpace(int i, int j){
           
-        if(i<0 || i >= boardwidth || j <0 || j >= boardheight || copyboard[i][j] != -1){
+        if(i<0 || i >= boardwidth || j <0 || j >= boardheight || copyboard[i][j]){
             return false;
         } else {
             return true;
@@ -88,26 +75,26 @@ public final class TourSolver implements ITourSolver {
     }
 
 
-    public int[][] CopyBoard(Board board){
-        int[][] tmpcopyboard = new int[boardwidth][boardheight];
+    public boolean[][] CopyBoard(Board board){
+        boolean[][] tmpcopyboard = new boolean[boardwidth][boardheight];
 
         for(int i=0; i<boardwidth; i++){
             for(int j=0; j<boardheight; j++){
                 if(board.isMissing(i,j)){
-                    tmpcopyboard[i][j] = -2;
+                    tmpcopyboard[i][j] = true;
                 } else {
-                    tmpcopyboard[i][j] = -1;
+                    tmpcopyboard[i][j] = false;
                 }
             }
         }
         return tmpcopyboard;
     }
    
-    public int SetObjective(int[][] somecopyboard){
+    public int SetObjective(boolean[][] somecopyboard){
         int count = 0;
         for(int i=0; i<boardwidth; i++){
             for(int j=0; j<boardheight; j++){
-                if(somecopyboard[i][j] == -1){
+                if(!somecopyboard[i][j]){
                     count++;
                 } 
             }
